c3d57a72e16fb4f37e40e1e9eb016e01597e000e
iansosa <ian_sosa996@hotmail.com>
2021-05-27 04:12:42 -0300

    formating

--- Ensemble_Dinamic.h
+++ Ensemble_Dinamic.h
@@ -25,40 +25,40 @@ class Dinamic
 
     public:
 
-    class to_odeint
-    {
-		int &N_t_odeint;
-    	prec &K_t_odeint;
-
-    	Ensemble_param &I_t_odeint;
-    	Ensemble_param &F_t_odeint;
-    	Ensemble_connections &A_t_odeint;
-    	Ensemble_param &G_t_odeint;
-   	 	Ensemble_param &W_t_odeint;
-   	 public:
-   	 	to_odeint(int &N_in_t_odeint, prec &K_in_t_odeint,Ensemble_param &in_I_t_odeint,Ensemble_param &in_F_t_odeint,Ensemble_connections &in_A_t_odeint,Ensemble_param &in_G_t_odeint,Ensemble_param &in_W_t_odeint) : N_t_odeint(N_in_t_odeint), K_t_odeint(K_in_t_odeint),I_t_odeint(in_I_t_odeint),F_t_odeint(in_F_t_odeint),A_t_odeint(in_A_t_odeint),G_t_odeint(in_G_t_odeint),W_t_odeint(in_W_t_odeint)
-   	 	{}
-
-		prec interaction_sum_chain(int id,const std::vector<prec> &x);
-
-		prec interaction_sum(int id, const std::vector<prec> &x);
-
-		prec interaction_sum_all(int id, const std::vector<prec> &x);
-
-    	void operator() (const std::vector<prec> &x ,std::vector<prec> &dxdt ,const prec t)
+    	class to_odeint
     	{
-    		
-    		#pragma omp parallel for schedule(runtime)
-    	    for (int i = 0; i < N_t_odeint; ++i)
-    	    {
-    	    	prec sum;
-    	    	sum=interaction_sum(i,x);
-        		dxdt[2*i]=x[2*i+1];
-				dxdt[2*i+1]=sum/I_t_odeint[i]+F_t_odeint[i]*sin(W_t_odeint[i]*t-x[2*i])/I_t_odeint[i]-(G_t_odeint[i]/I_t_odeint[i])*x[2*i+1];      	
-        	}
-    	}
+			int &N_t_odeint;
+    		prec &K_t_odeint;
+	
+    		Ensemble_param &I_t_odeint;
+    		Ensemble_param &F_t_odeint;
+    		Ensemble_connections &A_t_odeint;
+    		Ensemble_param &G_t_odeint;
+   	 		Ensemble_param &W_t_odeint;
+   	 	public:
+   	 		to_odeint(int &N_in_t_odeint, prec &K_in_t_odeint,Ensemble_param &in_I_t_odeint,Ensemble_param &in_F_t_odeint,Ensemble_connections &in_A_t_odeint,Ensemble_param &in_G_t_odeint,Ensemble_param &in_W_t_odeint) : N_t_odeint(N_in_t_odeint), K_t_odeint(K_in_t_odeint),I_t_odeint(in_I_t_odeint),F_t_odeint(in_F_t_odeint),A_t_odeint(in_A_t_odeint),G_t_odeint(in_G_t_odeint),W_t_odeint(in_W_t_odeint)
+   	 		{}
 
-    };
+			prec interaction_sum_chain(int id,const std::vector<prec> &x);
+
+			prec interaction_sum(int id, const std::vector<prec> &x);
+
+			prec interaction_sum_all(int id, const std::vector<prec> &x);
+
+    		void operator() (const std::vector<prec> &x ,std::vector<prec> &dxdt ,const prec t)
+    		{
+    		
+    			#pragma omp parallel for schedule(runtime)
+    	    	for (int i = 0; i < N_t_odeint; ++i)
+    		    {
+    	 	   		prec sum;
+    		    	sum=interaction_sum(i,x);
+        			dxdt[2*i]=x[2*i+1];
+					dxdt[2*i+1]=sum/I_t_odeint[i]+F_t_odeint[i]*sin(W_t_odeint[i]*t-x[2*i])/I_t_odeint[i]-(G_t_odeint[i]/I_t_odeint[i])*x[2*i+1];      	
+        		}
+    		}
+
+    	};
 
 	
     	Dinamic(boost::mt19937 &in_rng) : rng(in_rng), A("A", in_rng),  I("I",in_rng), F("F",in_rng), G("G",in_rng), W("W",in_rng)
