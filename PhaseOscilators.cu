b8a78e60549eaa8ee628d6fe043dfc883a925f08
iansosa <ian_sosa996@hotmail.com>
2021-06-02 20:26:38 -0300

    example

--- PhaseOscilators.cu
+++ PhaseOscilators.cu
@@ -45,10 +45,8 @@ void check_stationary(Evolve &e, Dinamic &P)
 	e.print_init();
 }
 
-void printU(Evolve &e, Dinamic &P)
+void calc_U(Evolve &e, Dinamic &P, prec dx, std::vector<prec> &U)
 {
-	std::vector<prec> U(e.size(),0);
-
 	for (int i = 1; i < U.size(); ++i)
 	{
 		prec aux=e.Amp(i,P);
@@ -58,6 +56,18 @@ void printU(Evolve &e, Dinamic &P)
 		}
 	}
 
+	for (int i = 1; i < U.size()-1; ++i)
+	{
+		U[i]=(U[i+1]-U[i])/dx;
+	}
+}
+
+void printU(Evolve &e, Dinamic &P, prec dx)
+{
+	std::vector<prec> U(e.size(),0);
+
+	calc_U(e,P,dx,U);
+
 	std::string name="U";
 	mkdir("out", 0777);
 	std::ofstream txtOut;
@@ -66,18 +76,45 @@ void printU(Evolve &e, Dinamic &P)
 
 	for (int i = 1; i < U.size()-1; ++i)
 	{
-		U[i]=(U[i+1]-U[i])/0.0025;
-		prec diff=e.Diff(i);
-		if(diff>0)
-		{
-			std::cout <<"U(" << i << ")= " << U[i]<<"; Diff(" << i <<")= "<< diff << "; sin aprox(" << i << ")= " << 100*sin(diff)/diff << std::endl;
-			txtOut << i << " " << U[i] << " " << diff << " " << 100*sin(diff)/diff << std::endl;
-		}
+			txtOut << i << " " << U[i] << std::endl;
 	}
-	txtOut.close();
-	
+	txtOut.close();	
 }
 
+void print_U_stats(std::vector<std::vector<prec>> &U)
+{
+	std::vector<prec> U_med(U[0].size(),0);
+	std::vector<prec> U_sigm(U[0].size(),0);
+	for (int i = 0; i < U[0].size(); ++i) //N
+	{
+		for (int j = 0; j < U.size(); ++j) //promedio
+		{
+			U_med[i]=U_med[i]+U[j][i];
+		}
+		U_med[i]=U_med[i]/U.size();
+	}
+
+	for (int i = 0; i < U[0].size(); ++i) //N
+	{
+		for (int j = 0; j < U.size(); ++j) //promedio
+		{
+			U_sigm[i]=pow(U[j][i]-U_med[i],2);
+		}
+		U_sigm[i]=U_sigm[i]/U.size();
+		U_sigm[i]=sqrt(U_sigm[i]);
+	}
+	std::string name="U_stats";
+	mkdir("out", 0777);
+	std::ofstream txtOut;
+	txtOut.open("out/"+name+".txt");
+	txtOut.precision(std::numeric_limits< prec >::max_digits10);
+
+	for (int i = 0; i < U_med.size(); ++i)
+	{
+			txtOut << i << " " << U_med[i] << " " << U_sigm[i] << std::endl;
+	}
+	txtOut.close();	
+}
 
 int main()
 {
@@ -87,30 +124,36 @@ int main()
  	std::cout << "N: ";
  	std::cin >> N;
 
+  	int prom;
+ 	std::cout << "Prom: ";
+ 	std::cin >> prom;
+
+ 	std::vector<std::vector<prec> > U(prom);
+
     int chunk_size = N/omp_get_max_threads();
     omp_set_schedule( omp_sched_static , chunk_size );
 
-    prec K=16*N;
+    prec conv_factor=5;
+    prec dx=0.01;
+    dx=dx/conv_factor;
+
+    prec K=conv_factor*conv_factor*N;
     prec I=0.1;
     prec G=5;
-	Dinamic P(N ,K ,rng ,I ,0 ,1 ,0 ,G ,0 ,1 ,0,"chain"); //N, K, rng, I, sigm_I, F, sigm_F, G, sigm_G, W, sigm_W
-	P.print_params();
+	Dinamic P(N ,K ,rng ,I ,I*0.01 ,1 ,0 ,G ,G*0.01 ,1 ,0,"chain"); //N, K, rng, I, sigm_I, F, sigm_F, G, sigm_G, W, sigm_W
 
-	
 	Evolve e(N, rng);
-	//check_stationary(e, P);
-	e.run(P,0,50000,100);
-	e.print();
-	for (int i = 1; i < N; ++i)
+	for (int i = 0; i < prom; ++i)
 	{
-		std::cout << i << ": " << e.Amp(i,P) << std::endl;
+		U[i].resize(N);
+		e.run(P,0,50000,100);
+		calc_U(e,P,dx,U[i]);
+		e.clean();
+
+		P.generate();
 	}
-	//e.print();
-	e.print_init();
-	printU(e,P);
 
-
-
+	print_U_stats(U);
 
 	return 0;
 }
